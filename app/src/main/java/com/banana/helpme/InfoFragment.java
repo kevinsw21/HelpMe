cf94f3e6cc451f3aee767c70717a1818667af20c
seishino <dhiazchebastian01@gmail.com>
2019-09-13 00:00:11 +0700

    tambah menu, main, fragment, belum set logic fragment

---
+++ app/src/main/java/com/banana/helpme/InfoFragment.java
@@ -0,0 +1,14 @@
+package com.banana.helpme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InfoFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_fragment);
+    }
+}
---
+++ app/src/main/java/com/banana/helpme/TipsFragment.java
@@ -0,0 +1,14 @@
+package com.banana.helpme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipsFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tips_fragment);
+    }
+}
