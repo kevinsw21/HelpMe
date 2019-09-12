932832cb73dc6d5c9d054a775956f7567e9694f9
seishino <dhiazchebastian01@gmail.com>
2019-09-13 01:37:41 +0700

    bottom navbar sukses, fragment jalan

--- app/src/main/java/com/banana/helpme/Login.java
+++ app/src/main/java/com/banana/helpme/Login.java
@@ -5,22 +5,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
     private TextView btnRegister;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         btnRegister = (TextView) findViewById(R.id.textRegister);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent reg = new Intent(Login.this, Register.class);
                 startActivity(reg);
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent log = new Intent(Login.this, MainActivity.class);
+                startActivity(log);
             }
         });
     }
--- app/src/main/java/com/banana/helpme/TipsFragment.java
+++ app/src/main/java/com/banana/helpme/TipsFragment.java
@@ -1,14 +1,20 @@
 package com.banana.helpme;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class TipsFragment extends AppCompatActivity {
-
+public class TipsFragment extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tips_fragment);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_tips_fragment, container, false);
+        return view;
     }
 }
